80bb755262cfca3ac96ef7085df2381d69182334
Charles DZADU <dzaducharles@gmail.com>
2025-04-04 16:20:12 +0000

    Remove unused files and directories, including .DS_Store, configuration files, models, controllers, views, and helper functions, to clean up the project structure.

--- src/controllers/report_controller.py
+++
@@ -1,2 +0,0 @@
-class ReportController:
-    """ Controller for reports """
--- src/helpers/constants.py
+++
@@ -1,4 +0,0 @@
-BASE_DB_PATH = "./data/tournaments"
-
-PLAYERS_DB_FILE = f"{BASE_DB_PATH}/players.json"
-TOURNAMENTS_DB_FILE = f"{BASE_DB_PATH}/tournaments.json"
